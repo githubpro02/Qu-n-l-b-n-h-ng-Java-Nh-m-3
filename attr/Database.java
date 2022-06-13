e951b9a9985141b7693e1f56d99155128a379ae3
Manh <nguyenducmanh11102002@gmail.com>
2022-06-13 14:45:48 +0700

    Add new project Quan ly ban hang Nhom 3

---
+++ attr/Database.java
@@ -0,0 +1,7 @@
+package attr;
+
+public class Database {
+	public static final String HOST_URI = "jdbc:mysql://localhost:3306/data_java";
+	public static final String USER = "root";
+	public static final String PASSWORD = "11102322002";
+}
